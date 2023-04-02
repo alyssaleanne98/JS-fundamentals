4af30cb7da6c7b41ef7ad96ac3535ab4a30d5ecc
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-04-02 12:25:27 -0700

    conditional expressions

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Hello World') // console log is a debugging tool used to log to the console 
+// node script.js - node is a way to execute js. 
+
+// Conditional expressions - In js we use true/truthy & false/falsey 
+
+if (NaN) {
+    console.log("truthy!")
+} else {
+    console.log("falsey!")
+}
+//we can easily test expressions (even strings) by typing it in the place of true
+// empty strings are falsey 
+// any string that has values inside is: truthy 
+// null is falsey
+// values that are falsey: false, zero, NaN, empty string 
+
+
+
+
+
